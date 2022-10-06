8da18828c71c64ed4e72ccee05e41ff7b41326e5
torone89 <torone89>
2022-10-06 16:12:48 +0200

    feat:complete style post publication

--- resources/views/mail/posts/publication.blade.php
+++ resources/views/mail/posts/publication.blade.php
@@ -9,27 +9,60 @@
 </head>
 
 <body>
-    <h1>
-        {{ $post->title }}
-    </h1>
-    <h4>
-        {{ $post->user->title }}
-    </h4>
-    <img :src="{{ $post->image }}" alt="">
-    <span>
-        Data pubblicazione: {{ $post->created_at }}
-    </span>
-    <p>
-        Categoria: {{ $post->category ? $post->category->label : 'Nessuna' }}
-    </p>
-    <p>
-        Tags:
-    </p>
-    @forelse($post->tags as $tag)
-        <div>{{ $tag->label }}</div>
-    @empty
-        Vuoto...
-    @endforelse
+    <header>
+        <h1>
+            {{ $post->title }}
+        </h1>
+    </header>
+    <div class="clearfix">
+        @if ($post->image)
+            <img class="float-left mr-2 img-fluid" width="500" src="{{ asset('storage/' . $post->image) }}"
+                alt="{{ $post->slug }}">
+        @endif
+        <p>
+            <strong>Categoria:</strong>
+            @if ($post->category)
+                {{ $post->category->label }}
+            @else
+                Nessuna
+            @endif
+        </p>
+        <div class="mb-2">
+            <strong>Tags:</strong>
+            @forelse($post->tags as $tag)
+                <span class="badge text-white p-1"
+                    style="background-color: {{ $tag->color }}">{{ $tag->label }}</span>
+            @empty
+                Nessun tag
+            @endforelse
+        </div>
+        <div>
+            <p>{{ $post->content }}</p>
+        </div>
+
+
+
+        <div><strong>Creato il:</strong> <time>{{ $post->created_at }}</time></div>
+        <div><strong> Modificato il:</strong> <time>{{ $post->updated_at }}</time></div>
+        <div>
+            @if ($post->user)
+                <strong>Autore:</strong> {{ $post->user->name }}
+            @else
+                Autore anonimo
+            @endif
+        </div>
+    </div>
+    <footer class="mt-5">
+        <div class="d-flex align-items-center justify-content-between">
+            <div>
+                <a href="{{ route('admin.posts.index') }}" class="btn btn-secondary">
+                    <i class="fa solid fa-rotate-left m-2"></i> Indietro
+                </a>
+
+
+            </div>
+
+            < </footer>
 </body>
 
 </html>
